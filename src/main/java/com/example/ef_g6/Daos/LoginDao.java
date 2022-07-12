1d5bee14348190af9a3b5cb13e21fa4aac8690f9
nipanaque <a20191018@pucp.edu.pe>
2022-07-12 14:10:45 -0500

    LoginDao

--- src/main/java/com/example/ef_g6/Daos/LoginDao.java
+++ src/main/java/com/example/ef_g6/Daos/LoginDao.java
@@ -1,7 +1,6 @@
 package com.example.ef_g6.Daos;
 
-import com.example.ef_g6.Beans.Empleado;
-import com.example.ef_g6.Beans.Rol;
+import com.example.ef_g6.Beans.*;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -9,56 +8,50 @@ import java.util.ArrayList;
 public class LoginDao extends DaoBase{
     public Integer getSal(Integer dni){
         Integer sal = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            String sql = "SELECT e.salario FROM empleado e WHERE e.dni = ?;";
-            Connection conn = this.obtenerConexion();
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "SELECT e.salario FROM empleado e WHERE e.dni = ?;";
+        try (Connection connection = this.obtenerConexion();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
             pstmt.setString(1, String.valueOf(dni));
-            ResultSet rs = pstmt.executeQuery(sql);
-            if (rs.next()){
-                sal = rs.getInt(1);
+
+            try(ResultSet rs = pstmt.executeQuery();){
+                if (rs.next()){
+                    sal = rs.getInt(1);
+                }
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return sal;
     }
     public Empleado getEmpleado(Integer dni){
         Empleado user = new Empleado();
         Rol rol = new Rol();
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        String sql = "SELECT e.nombre, e.apellido, r.nombre FROM empleado e, rolempleado re, rol r\n" +
+                "WHERE e.idempleado = re.idempleado\n" +
+                "AND re.idrol = r.idrol\n" +
+                "AND e.dni = ?;";
+        try (Connection connection = this.obtenerConexion();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
 
-        try {
-            String sql = "SELECT e.nombre, e.apellido, r.nombre FROM empleado e, rolempleado re, rol r\n" +
-                    "WHERE e.idempleado = re.idempleado\n" +
-                    "AND re.idrol = r.idrol\n" +
-                    "AND e.dni = ?;";
-            Connection conn = this.obtenerConexion();
-            PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, String.valueOf(dni));
-            ResultSet rs = pstmt.executeQuery(sql);
-            if (rs.next()){
-                user.setNombre(rs.getString(1));
-                user.setApellido(rs.getString(2));
-                rol.setIdRol(rs.getInt(3));
-                rol.setNombre(rs.getString(4));
-                ArrayList <Rol> roles = new ArrayList<>();
-                roles.add(rol);
-                user.setRoles(roles);
+
+            try(ResultSet rs = pstmt.executeQuery();){
+                if (rs.next()){
+                    user.setNombre(rs.getString(1));
+                    user.setApellido(rs.getString(2));
+                    rol.setIdRol(rs.getInt(3));
+                    rol.setNombre(rs.getString(4));
+                    ArrayList <Rol> roles = new ArrayList<>();
+                    roles.add(rol);
+                    user.setRoles(roles);
+                }
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return user;
     }
 }
+
+
