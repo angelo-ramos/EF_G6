911216b5f648a8f4f333a5229549d53858df6add
nipanaque <a20191018@pucp.edu.pe>
2022-07-12 12:07:09 -0500

    DaoBase

---
+++ src/main/java/Daos/DaoBase.java
@@ -0,0 +1,20 @@
+package Daos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DaoBase {
+    public Connection obtenerConexion() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        String username = "root";
+        String password = "root";
+        String url = "jdbc:mysql://localhost:3306/movies";
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
